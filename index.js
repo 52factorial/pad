f0eb7c7caa2350603145a3e12c13b66ac9071bef
Fabio Machado <fabiohsmachado@gmail.com>
2016-09-14 03:49:46 -0300

    Refactored whole serve to be multipurpose

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-var http = require('http'),
-    fs = require('fs'),
-    port = process.env.PORT || 8000;
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
 
-fs.readFile('./html/pad.html',
- function (err, html) {
-  if (err) {
-   throw err;
-  }
-  http.createServer(function(request, response) {
-   response.writeHeader(200, {"Content-Type": "text/html"});
-   response.write(html);
-   response.end();
-  }
- ).listen(port);
-});
+var port = process.env.PORT || 8000;
+
+http.createServer(function(request, response){
+    fs.readFile('./html/' + request.url, function(error, data){
+        if(!error) {
+            var dotoffset = request.url.lastIndexOf('.');
+            var mimetype = dotoffset == -1 ? 'text/plain' :
+                {
+                    '.html' : 'text/html',
+                    '.css' : 'text/css',
+                    '.js' : 'text/javascript'
+                }[request.url.substr(dotoffset)];
+            response.writeHeader(200, {"Content-type": mimetype});
+            response.write(data);
+            console.log("Served file: ", request.url, mimetype);
+        } else {
+            console.log("File not found: " + request.url);
+            response.writeHead(404, "Not Found");
+        }
+        response.end();
+    })
+}).listen(port);
